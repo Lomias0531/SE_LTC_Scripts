3ef161dcb84ca783f36b48f80bb8a1fe9b1ed969
Lomias0531 <460492314@qq.com>
2021-08-31 18:06:56 +0800

    Create LTC_PlanetArtillery.cs

---
+++ SEScript/LTC_PlanetArtillery.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sandbox.ModAPI.Ingame;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using VRageMath;
+
+namespace SEScript
+{
+    class LTC_PlanetArtillery:API
+    {
+        //Physical variables
+        static float shellVelocity = 0;
+        Vector3D curGravity
+        {
+            get
+            {
+                return artilleryComputer.GetNaturalGravity();
+            }
+        }
+        Vector3D targetPos;
+        Vector3D ComputerPos;
+        //Controls
+        IMyRemoteControl artilleryComputer;
+        IMyMotorStator verticalRot;
+        IMyMotorStator verticalRotRev;
+        IMyMotorStator horizontalRot;
+        IMyTimerBlock Trigger;
+        float calculateAngle(Vector3D receivedTarget)
+        {
+            float angle = 0;
+            return angle;
+        }
+        void Main(string arg,UpdateType updateSource)
+        {
+            if(updateSource == UpdateType.IGC)
+            {
+                
+            }
+        }
+        void CheckComponents()
+        {
+
+        }
+    }
+}
