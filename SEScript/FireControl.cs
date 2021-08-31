4d9b8caa872a7ddad175e9f26eefa51763c086b1
Lomias0531 <56434685+Lomias0531@users.noreply.github.com>
2021-08-31 08:05:58 +0800

    12

---
+++ SEScript/FireControl.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sandbox.ModAPI.Ingame;
+
+namespace SEScript
+{
+    class FireControl:API
+    {
+        List<IMyBlockGroup> CannonTurret;
+    }
+}
---
+++ SEScript/TurretControl.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sandbox.ModAPI.Ingame;
+
+namespace SEScript
+{
+    class TurretControl:API
+    {
+
+    }
+}
