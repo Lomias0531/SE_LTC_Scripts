63d09a1809848ed1aa26b8efbe3a2c78e1a5bcb8
Lomias0531 <460492314@qq.com>
2021-10-11 17:31:15 +0800

    Update LTC_Veil.cs

--- SEScript/LTC_Veil.cs
+++ SEScript/LTC_Veil.cs
@@ -1,10 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Sandbox.ModAPI.Ingame;
+using SpaceEngineers.Game.ModAPI;
 
 namespace SEScript
 {
-    class LTC_Veil
+    class LTC_Veil:API
     {
+        bool CheckReady = false;
+        List<IMyGravityGeneratorSphere> GravityRestraint_In;
+        List<IMyGravityGeneratorSphere> GravityRestraint_Out;
+        List<IMyGravityGenerator> GravityControls;
+        IMyGravityGenerator NormalGravity;
+        List<IMyShipConnector> StoneSpitter;
+        List<IMyCargoContainer> Cargos;
+        void Main()
+        {
+            if(!CheckReady)
+            {
+                CheckComponents();
+                return;
+            }
+        }
+        void CheckComponents()
+        {
+            GravityRestraint_In = new List<IMyGravityGeneratorSphere>();
+            GravityRestraint_Out = new List<IMyGravityGeneratorSphere>();
+            GravityControls = new List<IMyGravityGenerator>();
+            StoneSpitter = new List<IMyShipConnector>();
+            Cargos = new List<IMyCargoContainer>();
+        }
     }
 }
